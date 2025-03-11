3eef088d35283b7c118536bf0bfbb8006e391b56
Blonbar <117039186+jonstreeter@users.noreply.github.com>
2025-03-11 11:36:22 -0500

    Add files via upload
    current snapshot - still buggy

--- __init__.py
+++ __init__.py
@@ -1,1 +1,14 @@
+import os
+import sys
 
+# Add the parent directory of 'models' and 'utils' to the Python path
+current_dir = os.path.dirname(os.path.abspath(__file__))
+if current_dir not in sys.path:
+    sys.path.append(current_dir)
+
+from .DeepExemplarImageColorNode import NODE_CLASS_MAPPINGS as IMG_MAP
+from .DeepExemplarVideoColorNode import NODE_CLASS_MAPPINGS as VID_MAP
+
+NODE_CLASS_MAPPINGS = {}
+NODE_CLASS_MAPPINGS.update(IMG_MAP)
+NODE_CLASS_MAPPINGS.update(VID_MAP)
